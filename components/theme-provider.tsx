9d26c23608121e66dde79869d45d0c6fa5cafe14
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2025-06-22 21:36:03 +0900

    feat: enable system theme

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,7 +4,7 @@ import type { ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider defaultTheme="light" {...props}>
+    <NextThemesProvider enableSystem defaultTheme="light" {...props}>
       {children}
     </NextThemesProvider>
   );
