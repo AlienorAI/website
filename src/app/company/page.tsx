39b0accd54514ec009dd2e73bcb4ae31b33cb477
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2025-08-18 15:02:44 +0900

    feat: add new pictures

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -139,7 +139,7 @@ function Person({
 }) {
   return (
     <li className="flex items-center gap-4">
-      <img alt="" src={img} className="size-12 rounded-full object-cover" />
+      <img alt="" src={img} className="size-18 rounded-full object-cover" />
       <div className="text-sm/6">
         <h3 className="font-medium">{name}</h3>
         <p className="text-gray-500">{description}</p>
--- src/components/logo-cloud.tsx
+++ src/components/logo-cloud.tsx
@@ -1,40 +1,30 @@
-import { clsx } from 'clsx'
+import { clsx } from "clsx";
 
 export function LogoCloud({
   className,
-}: React.ComponentPropsWithoutRef<'div'>) {
+}: React.ComponentPropsWithoutRef<"div">) {
   return (
     <div
       className={clsx(
         className,
-        'flex justify-between max-sm:mx-auto max-sm:max-w-md max-sm:flex-wrap max-sm:justify-evenly max-sm:gap-x-4 max-sm:gap-y-4',
+        "mx-auto flex max-w-3xl flex-wrap items-center justify-between",
       )}
     >
       <img
-        alt="SavvyCal"
-        src="/logo-cloud/savvycal.svg"
+        alt={"Logo de la French Tech"}
+        src="/logo-cloud/french-tech.webp"
+        className="h-9 max-sm:mx-auto sm:h-8 lg:h-20"
+      />
+      <img
+        alt={"Logo de la BPI France"}
+        src="/logo-cloud/bpi-france.svg"
         className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
       />
       <img
-        alt="Laravel"
-        src="/logo-cloud/laravel.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
-      <img
-        alt="Tuple"
-        src="/logo-cloud/tuple.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
-      <img
-        alt="Transistor"
-        src="/logo-cloud/transistor.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
-      <img
-        alt="Statamic"
-        src="/logo-cloud/statamic.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
+        alt={"Logo de la Nouvelle Aquitaine France"}
+        src="/logo-cloud/nouvelle-aquitaine.svg"
+        className="h-9 max-sm:mx-auto sm:h-8 lg:h-24"
       />
     </div>
-  )
+  );
 }
